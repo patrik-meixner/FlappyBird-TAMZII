2a14a5cab8bffd8e24efb3f8f6580dd5b8f80040
patrik_meixner <patrik.meixner98@gmail.com>
2020-12-08 21:37:13 +0100

    feat(project): numbers images, floor component

---
+++ app/src/main/java/com/example/componets/Floor.java
@@ -0,0 +1,54 @@
+package com.example.componets;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+public class Floor {
+
+    private Bitmap bar1;
+    private Bitmap bar2;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    private int outOfScreen;
+
+    public Floor(Context context, int x, int y, int width) {
+        Bitmap bar = null;
+
+        try {
+
+            bar = BitmapFactory.decodeStream(context.getAssets().open("floor.png"));
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+        this.bar1 = Bitmap.createScaledBitmap(bar, width, 70, true);
+        this.bar2 = Bitmap.createScaledBitmap(bar, width + 6, 70, true);
+        this.x1 = x;
+        this.y1 = y;
+        this.x2 = width - 12;
+        this.y2 = y;
+        this.outOfScreen = width;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(this.bar1, this.x1, this.y1, null);
+        canvas.drawBitmap(this.bar2, this.x2, this.y2, null);
+    }
+
+    public void move() {
+        if (this.x2 <= 0 && this.x1 <= -133) {
+            this.x1 = this.outOfScreen - 12;
+        }
+        if (this.x1 <= 0 && this.x2 <= -133) {
+            this.x2 = this.outOfScreen - 12;
+        }
+        this.x1 -= 8;
+        this.x2 -= 8;
+    }
+}
