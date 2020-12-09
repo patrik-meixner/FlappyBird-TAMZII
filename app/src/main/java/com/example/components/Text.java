b86ee6e303cf698a52a202eb18040710e2dc745c
patrik_meixner <patrik.meixner98@gmail.com>
2020-12-09 11:17:50 +0100

    feat(project): text adjustments

--- app/src/main/java/com/example/components/Text.java
+++ app/src/main/java/com/example/components/Text.java
@@ -11,33 +11,46 @@ import com.example.flappybird.R;
 public class Text {
 
     private String text;
-    private Paint paint;
+    private Paint strokePaint;
+    private Paint fillPaint;
     private int x, y;
 
     public Text(Context context, int x, int y, String text) {
-        this.x = x;
-        this.y = y;
-        this.text = text;
-        Typeface font = ResourcesCompat.getFont(context, R.font.flappybird);
-        this.paint = new Paint();
-        this.paint.setTypeface(font);
-        this.paint.setARGB(255, 0, 0, 0);
-        this.paint.setTextSize(130);
+        setCommonTextProperties(context, x, y, text);
+        this.strokePaint.setTextSize(130);
+        this.fillPaint.setTextSize(130);
     }
 
     public Text(Context context, int x, int y, String text, int size) {
+        setCommonTextProperties(context, x, y, text);
+        this.strokePaint.setTextSize(size);
+        this.fillPaint.setTextSize(size);
+    }
+
+    public void setCommonTextProperties(Context context, int x, int y, String text) {
+        Typeface font = ResourcesCompat.getFont(context, R.font.flappybird);
+        this.strokePaint = new Paint();
+        this.fillPaint = new Paint();
+
         this.x = x;
         this.y = y;
+
         this.text = text;
-        Typeface font = ResourcesCompat.getFont(context, R.font.flappybird);
-        this.paint = new Paint();
-        this.paint.setTypeface(font);
-        this.paint.setARGB(255, 0, 0, 0);
-        this.paint.setTextSize(size);
+
+        this.strokePaint.setStyle(Paint.Style.STROKE);
+        this.strokePaint.setStrokeWidth(15);
+        this.strokePaint.setTypeface(font);
+        this.strokePaint.setARGB(255, 0, 0, 0);
+
+        this.fillPaint.setStyle(Paint.Style.FILL);
+        this.fillPaint.setTypeface(font);
+        this.fillPaint.setARGB(255, 255, 255, 255);
+        this.fillPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawText(this.text, this.x, this.y, this.paint);
+        canvas.drawText(this.text, this.x, this.y, this.strokePaint);
+        canvas.drawText(this.text, this.x, this.y, this.fillPaint);
     }
 
     public void setText(String text) {
